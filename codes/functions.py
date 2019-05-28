eae04711d88b5a926e6cf1688a883bcc676f02e8
Nikita Kozodoi <n.kozodoi@icloud.com>
2019-05-28 10:43:39 +0200

    correct aggregations

--- codes/functions.py
+++ codes/functions.py
@@ -78,17 +78,20 @@ def predict_proba_with_tta(X_test, model, num_iteration, alpha = 0.01, n = 4, se
 
 # performs dummy or label encoding
 
-def encode_factors(df, method = "label"):
+def encode_factors(df, method = "label", skip = None):
+    
+    # select columns 
+    factors = [f for f in df.columns if df[f].dtype == "object"]
+    factors = [f for f in factors if f not in skip]
     
     # label encoding
     if method == "label":
-        factors = [f for f in df.columns if df[f].dtype == "object"]
         for var in factors:
             df[var], _ = pd.factorize(df[var])
         
     # dummy encoding
     if method == "dummy":
-        df = pd.get_dummies(df, drop_first = True)
+        df = pd.get_dummies(df, drop_first = True, columns = factors)
     
     # dataset
     return df
@@ -125,8 +128,7 @@ def count_missings(data):
 # returns df with generated features
 
 import scipy.stats
-def aggregate_data(df, group_var, num_stats = ['mean', 'sum'],
-                   var_label = None, sd_zeros = False):
+def aggregate_data(df, group_var, num_stats = ['mean', 'sum'], factors = None, var_label = None, sd_zeros = False):
     
     
     ### SEPARATE FEATURES
@@ -135,8 +137,11 @@ def aggregate_data(df, group_var, num_stats = ['mean', 'sum'],
     print("- Preparing the dataset...")
 
     # find factors
-    df_factors = [f for f in df.columns if df[f].dtype == "object"]
-    df_factors = ['group_id']
+    if factors == None:
+        df_factors = [f for f in df.columns if df[f].dtype == "object"]
+    else:
+        df_factors = factors
+        df_factors.append(group_var)
         
     # partition subsets
     if type(group_var) == str:
@@ -144,39 +149,33 @@ def aggregate_data(df, group_var, num_stats = ['mean', 'sum'],
         fac_df = df[df_factors]
     else:
         num_df = df[group_var + list(set(df.columns) - set(df_factors))]
-        fac_df = df[df_factors]      
+        fac_df = df[df_factors] 
     
     # display info
     num_facs = fac_df.shape[1] - 1
     num_nums = num_df.shape[1] - 1
     print("- Extracted %.0f factors and %.0f numerics..." % (num_facs, num_nums))
-
+    
 
     ##### AGGREGATION
  
     # aggregate numerics
     if (num_nums > 0):
         print("- Aggregating numeric features...")
-        if type(group_var) == str:
-            num_df = num_df.groupby([group_var]).agg(num_stats)
-            num_df.columns = ["_".join(col).strip() for col in num_df.columns.values]
-            num_df = num_df.sort_index()
-        else:
-            num_df = num_df.groupby(group_var).agg(num_stats)
-            num_df.columns = ["_".join(col).strip() for col in num_df.columns.values]
-            num_df = num_df.sort_index()
+        num_df = num_df.groupby([group_var]).agg(num_stats)
+        num_df.columns = ["_".join(col).strip() for col in num_df.columns.values]
+        num_df = num_df.sort_index()
 
     # aggregate factors
     if (num_facs > 0):
         print("- Aggregating factor features...")
-        if type(group_var) == str:
-            fac_df = fac_df.groupby([group_var]).agg([("mode", lambda x: scipy.stats.mode(x)[0][0])])
-            fac_df.columns = ["_".join(col).strip() for col in fac_df.columns.values]
-            fac_df = fac_df.sort_index()
-        else:
-            fac_df = fac_df.groupby(group_var).agg([("mode", lambda x: scipy.stats.mode(x)[0][0])])
-            fac_df.columns = ["_".join(col).strip() for col in fac_df.columns.values]
-            fac_df = fac_df.sort_index()
+        fac_int_df = fac_df.copy()
+        for var in factors:
+            fac_int_df[var], _ = pd.factorize(fac_int_df[var])
+        fac_int_df[group_var] = fac_df[group_var]
+        fac_df = fac_int_df.groupby([group_var]).agg([('count'), ('mode', lambda x: pd.Series.mode(x)[0])])
+        fac_df.columns = ["_".join(col).strip() for col in fac_df.columns.values]
+        fac_df = fac_df.sort_index()           
 
 
     ##### MERGER
